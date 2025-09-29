9e43b967db860ca33db07388736cbdeb3cd92f52
DoubleS <geg98779@gmail.com>
2025-09-29 15:36:25 +0800

    feat: 完成设备的响应式判断

--- master/src/App.vue
+++ master/src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
-    <img class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none" src="https://picsum.photos/200/300" alt="">
-    <div class="text-lg "> 
-      <p class="text-zinc-100">Lorem, ipsum dolor.
-      Fugit, maiores at!
-      Ut, excepturi incidunt!
-      Eos, delectus alias.
-      Ea, ab laudantium?</p>
-  </div>
-  </div>
-  
+  <div>{{ isMoblieTerminal }}</div>
 </template>
 
 <script setup>
+import { isMoblieTerminal } from './utils/flexible';
+
 
 </script>
---
+++ master/src/utils/flexible.js
@@ -0,0 +1,11 @@
+/**
+ * 判断当前设备是否为移动端设备
+ */
+import { PC_DEVICE_WIDTH } from "../constants";
+import { computed } from "vue";
+import { useWindowSize } from "@vueuse/core";
+
+const { width } = useWindowSize()
+export const isMoblieTerminal = computed(() => {
+    return width.value < PC_DEVICE_WIDTH
+})
