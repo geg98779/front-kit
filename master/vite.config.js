d7c0c9e64e55aca0078153528b9dd326412f8df1
DoubleS <geg98779@gmail.com>
2025-09-29 15:40:23 +0800

    feat: vite中配置好@软链接

--- master/vite.config.js
+++ master/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {join} from 'path'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
-  //软链接
+  //vite中配置@软链接
   resolve:{
     alias:{
       '@': join(__dirname, '/src')
