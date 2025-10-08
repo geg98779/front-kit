40e6ba592c27d39e0f790643ef28a3ce6ac4d46d
DoubleS <geg98779@gmail.com>
2025-10-08 21:51:59 +0800

    feat: mobile navigationBar

--- master/src/views/main/components/navigation/index.vue
+++ master/src/views/main/components/navigation/index.vue
@@ -1,10 +1,19 @@
 <template>
-    <mobile-navigation-vue v-if="isMobileTerminal"/>
+    <mobile-navigation-vue v-if="isMobileTerminal" :data="categoryData"/>
 </template>
 
 <script setup>
-    import mobileNavigationVue from './mobile/index.vue'
-    import { isMobileTerminal } from '@/utils/flexible.js'
+import mobileNavigationVue from './mobile/index.vue'
+import { isMobileTerminal } from '@/utils/flexible.js'
+import {ref} from 'vue'
+import { getCategory } from '@/api/category';
+
+const categoryData = ref([])
+const getCategoryData = async () => {
+    const { categorys } = await getCategory()
+    categoryData.value = categorys
+}
+getCategoryData()
 </script>
 
 <style lang="scss" scoped>
