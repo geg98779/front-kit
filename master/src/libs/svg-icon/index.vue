f98cb7b82b280c88ebd00bb3d653682f0ec2838f
DoubleS <geg98779@gmail.com>
2025-10-09 19:55:16 +0800

    feat: 通用组件 m-svg-icon

---
+++ master/src/libs/svg-icon/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg aria-hidden="true">
+        <use :xlink:href = "symbolId" :class="fillClass" :fill="color"></use>
+    </svg>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+    name:{
+        type: String,
+        required: true
+    },
+    color:{
+        type: String
+    },
+    fillClass:{
+        type: String
+    }
+})
+
+const symbolId = computed(() => `#icon-${props.name}`)
+</script>
+
--- master/src/main.js
+++ master/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.scss'
 import router from './router'
+import mLibs from './libs'
 import { useREM } from './utils/flexible'
 
 useREM()
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(mLibs).mount('#app')
