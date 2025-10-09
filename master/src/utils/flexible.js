1be02f643c5140d181c6f968500c7cce2b764d49
DoubleS <geg98779@gmail.com>
2025-10-09 17:22:56 +0800

    feat: 完善动态rem功能 配置tailwindfontSize

--- master/src/utils/flexible.js
+++ master/src/utils/flexible.js
@@ -6,6 +6,21 @@ import { computed } from "vue";
 import { useWindowSize } from "@vueuse/core";
 
 const { width } = useWindowSize()
+
 export const isMobileTerminal = computed(() => {
     return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ * 动态计算rem基准值 根据用户设备屏幕宽度来计算 并把结果赋给html根标签作为fontSize大小
+ */
+
+export const useREM = () => {
+    const MAX_FONT_SIZE = 40
+    document.addEventListener('DOMContentLoaded', () => {
+        const html = document.querySelector('html')
+        let fontSize = window.innerWidth / 10
+        fontSize = MAX_FONT_SIZE < fontSize? MAX_FONT_SIZE : fontSize
+        html.style.fontSize = fontSize + 'px'
+    })
+}
