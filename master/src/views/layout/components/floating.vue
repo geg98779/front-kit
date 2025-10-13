7a502b687023882889e3c4a953081990862d8d39
DoubleS <geg98779@gmail.com>
2025-10-13 18:17:04 +0800

    feat: 实现pc端布局基础架构

---
+++ master/src/views/layout/components/floating.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="fixed bottom-10 right-2">
+        悬浮区
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ master/src/views/layout/components/main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        主要内容区域        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
