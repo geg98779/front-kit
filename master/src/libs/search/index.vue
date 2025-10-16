b2f70147658a27818659d899d472282d5c62cb0e
DoubleS <geg98779@gmail.com>
2025-10-16 22:14:47 +0800

    feat: 完善通用组件search搜索框

--- master/src/libs/search/index.vue
+++ master/src/libs/search/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40"
+    ref="containerTarget"
   >
     <!-- search部分 -->
     <div>
@@ -17,24 +18,38 @@
          caret-zinc-400 rounded-xl text-zinc-400 tracking-wide
           text-sm font-semibold border border-zinc-100  focus:border-red-300
           group-hover:bg-white group-hover:border-zinc-200"
-        placeholder="搜索" 
+        placeholder="搜索"
+        v-model="inputValue"
+        @keyup.enter="onSearchHandler"
+        @focus="onFocusHandler"
+        @blur="onBlurHandler"
       />
       <!-- 删除按钮 -->
-      <m-svg-icon name="input-delete" class="h-1.5 w-1.5 
-      absolute translate-y-[-50%] top-[50%] right-9 
-      cursor-pointer duration-500"></m-svg-icon>
+      <m-svg-icon
+      v-show="inputValue"
+      name="input-delete" class="h-1.5 w-1.5
+      absolute translate-y-[-50%] top-[50%] right-9
+      cursor-pointer duration-500"
+      @click="onClearClick"></m-svg-icon>
       <!-- 分割线 -->
       <div class="opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%]
       right-[62px] duration-500 bg-zinc-300 group-hover:opacity-100"></div>
       <!-- 搜索按钮（通用组件） -->
+      <m-button class="opacity-0 group-hover:opacity-100 duration-500 absolute translate-y-[-50%]
+       top-[50%] right-1 rounded-full" icon="search" iconColor="#ffffff"
+       @click="onSearchHandler" >
+      </m-button>
     </div>
     <!-- 下拉区 -->
     <transition name="slide">
-        <div class="max-h-[368px] w-full text-base overflow-auto bg-white
+        <div
+        v-if="$slots.dropdown"
+        v-show="isFocus"
+        class="max-h-[368px] w-full text-base overflow-auto bg-white
         absolute z-20 top-[56px] left-0 p-2 rounded border border-zinc-200
         duration-200 hover:shadow-2xl">
-        <!-- <slot name="dropdown" /> -->
-    </div>
+            <slot name="dropdown" />
+        </div>
     </transition>
   </div>
 </template>
@@ -57,6 +72,9 @@ export default {
 </script>
 
 <script setup>
+import { onClickOutside, useVModel } from '@vueuse/core'
+import { ref, useSlots, watch } from 'vue'
+const slots = useSlots()
 /**
  * 1. 输入内容实现双向数据绑定
  * 2. 搜索按钮在 hover 时展示
@@ -65,6 +83,56 @@ export default {
  * 5. 下拉展示区控制
  * 6. 事件处理
  */
+const props = defineProps({
+  modelValue: {
+    required: true,
+    type: String,
+  }
+})
+
+const emits = defineEmits([
+  EMIT_UPDATA_MODEL_VALUE,
+  EMIT_SEARCH,
+  EMIT_CLEAR,
+  EMIT_INPUT,
+  EMIT_FOCUS,
+  EMIT_BLUR
+])
+const inputValue = useVModel(props, 'modelValue', emits)
+//监听用户输入行为
+watch(inputValue, (val) => {
+  emits(EMIT_INPUT, val)
+})
+
+//删除文本
+const onClearClick = () => {
+  inputValue.value = ''
+  emits(EMIT_CLEAR, '')
+}
+
+//搜索
+const onSearchHandler = () => {
+  emits(EMIT_SEARCH, inputValue.value)
+}
+
+//获取焦点
+const isFocus = ref(false)
+const containerTarget = ref(null)
+
+const onFocusHandler = () => {
+  isFocus.value = true
+  emits(EMIT_FOCUS)
+}
+
+//失去焦点
+const onBlurHandler = () => {
+  emits(EMIT_BLUR)
+}
+
+//点击区域外隐藏dropdown
+onClickOutside(containerTarget, () => {
+  isFocus.value = false
+})
 </script>
 
 <style lang="scss" scoped>
