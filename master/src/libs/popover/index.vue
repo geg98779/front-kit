596d3b36404e5dd79518d8c77f1d65cf111a19e4
DoubleS <geg98779@gmail.com>
2025-10-19 18:00:43 +0800

    fix: 通过通过定时器防抖，修复popover慢速移动导致关闭问题

--- master/src/libs/popover/index.vue
+++ master/src/libs/popover/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative" @mouseenter="onMouseenter" @mouseleave="onMouseleave">
-    <div>
+    <div ref="referenceTarget">
       <!-- 具名插槽：用于触发弹层 -->
       <slot name="reference" />
     </div>
@@ -8,7 +8,9 @@
     <transition>
       <div
         v-show="isVisible"
+        ref="contentTarget"
         class="absolute p-1 z-20 bg-white border rounded-md"
+        :style="contentStyle"
       >
         <!-- 匿名插槽：用于展示弹出层的内容 -->
         <slot></slot>
@@ -17,19 +19,121 @@
   </div>
 </template>
 
+<script>
+// 延时关闭时间
+const DELAY_TIME = 200
+const PROP_TOP_LEFT = 'top-left'
+const PROP_TOP_RIGHT = 'top-right'
+const PROP_BOTTOM_LEFT = 'bottom-left'
+const PROP_BOTTOM_RIGHT = 'bottom-right'
+
+const placeEnum = [
+  PROP_TOP_LEFT,
+  PROP_TOP_RIGHT,
+  PROP_BOTTOM_LEFT,
+  PROP_BOTTOM_RIGHT
+]
+</script>
+
 <script setup>
-import { ref } from 'vue'
+/**
+ * 1. 指定多有可选位置的常亮，生成 enum
+ * 2. 通过 prop 控制指定位置
+ * 3. 获取元素的 DOM，创建读取元素尺寸的方法
+ * 4. 计算弹层位置
+ * 5. 根据 prop，计算样式对象
+ */
+import { ref, watch, nextTick, useSlots } from 'vue'
+const slots = useSlots()
+const props = defineProps({
+  // 气泡弹出位置，位置不对给出错误提示
+  placement: {
+    type: String,
+    default: PROP_BOTTOM_LEFT,
+    validator: (value) => {
+      const res = placeEnum.includes(value)
+      if (!res) {
+        throw new Error(`placement 必须为 ${placeEnum.join('、')} 中的一个`)
+      }
+      return res
+    }
+  }
+})
+
+const referenceTarget = ref(null)
+const contentTarget = ref(null)
+
+/**
+ * 获取元素尺寸
+ */
+const getElementSize = (target) => {
+  if (!target) return {}
+  return {
+    width: target.offsetWidth,
+    height: target.offsetHeight
+  }
+}
+
+const contentStyle = ref({
+  top: 0,
+  left: 0
+})
 
 const isVisible = ref(false)
 
+/**
+ * 计算弹层位置
+ * 在气泡展示的时候再进行计算
+ */
+watch(isVisible, (value) => {
+  if (!value) return
+  // 等待元素渲染
+  nextTick(() => {
+    switch (props.placement) {
+      // 左上
+      case PROP_TOP_LEFT:
+        contentStyle.value.top = 0
+        contentStyle.value.left =
+          -getElementSize(contentTarget.value).width + 'px'
+        break
+      // 右上
+      case PROP_TOP_RIGHT:
+        contentStyle.value.top = 0
+        contentStyle.value.left =
+          getElementSize(referenceTarget.value).width + 'px'
+        break
+      // 左下
+      case PROP_BOTTOM_LEFT:
+        contentStyle.value.top =
+          getElementSize(referenceTarget.value).height + 'px'
+        contentStyle.value.left =
+          -getElementSize(contentTarget.value).width + 'px'
+        break
+      // 右下
+      case PROP_BOTTOM_RIGHT:
+        contentStyle.value.top =
+          getElementSize(referenceTarget.value).height + 'px'
+        contentStyle.value.left =
+          getElementSize(referenceTarget.value).width + 'px'
+        break
+    }
+  })
+})
+
+let timer
 const onMouseenter = () => {
   isVisible.value = true
-  console.log('aklsdjfkaj')
+  if (timer) {
+    clearTimeout(timer)
+  }
 }
 
+//延迟关闭
 const onMouseleave = () => {
-  isVisible.value = false
-  console.log('leave')
+  timer = setTimeout(() => {
+    isVisible.value = false
+    timer = null
+  }, DELAY_TIME)
 }
 </script>
 
