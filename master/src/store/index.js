4ba72dd0fc93b000ee3538a4855b85dbbac3e7cb
DoubleS <geg98779@gmail.com>
2025-10-20 18:34:58 +0800

    feat: 使用persistedstate持久化缓存数据

--- master/src/store/index.js
+++ master/src/store/index.js
@@ -1,12 +1,18 @@
 import { createStore } from 'vuex'
 import getters from './getters'
 import category from './modules/category'
-
+import createPersistedstate from 'vuex-persistedstate'
 const store = createStore({
     getters,
     modules: {
         category
-    }
+    },
+    plugins: [
+        createPersistedstate({
+            key: 'front-kit',
+            paths: ['category']
+        })
+    ]
 })
 
 export default store
